6d63c8c922f21ee9c1db7ddf2b17311aa9d69d12
rhys <rhysormond@gmail.com>
2019-03-25 10:19:16 -0700

    Commit to fish for system-specific env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -84,28 +84,5 @@ function fish_prompt
     set_color normal
 end
 
-# reuse common env vars
-# from https://gist.github.com/overtrue/f7cd321708ba917b8def
-egrep "^export " ~/.env | while read e
-    set var (echo $e | sed -E "s/^export ([A-Za-z_]+)=(.*)\$/\1/")
-    set value (echo $e | sed -E "s/^export ([A-Za-z_]+)=(.*)\$/\2/")
-
-    # remove surrounding quotes if existing
-    set value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
-
-    if test $var = "PATH"
-        # replace ":" by spaces. this is how PATH looks for Fish
-        set value (echo $value | sed -E "s/:/ /g")
-
-        # use eval because we need to expand the value
-        eval set -xg $var $value
-
-        continue
-    end
-
-    # evaluate variables. we can use eval because we most likely just used "$var"
-    set value (eval echo $value)
-
-    #echo "set -xg '$var' '$value' (via '$e')"
-    set -xg $var $value
-end
+# system-specific env vars
+source $HOME/.env
