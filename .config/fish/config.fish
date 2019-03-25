b639bac9913bad168f3aac81223403547865777a
rhys <rhysormond@gmail.com>
2019-03-24 17:09:57 -0700

    Source common env in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -62,3 +62,29 @@ set __fish_git_prompt_showuntrackedfiles "yes"
 set __fish_git_prompt_showdirtystate "yes"
 set __fish_git_prompt_showstashstate ""
 set __fish_git_prompt_showupstream "none"
+
+# reuse common env vars
+# from https://gist.github.com/overtrue/f7cd321708ba917b8def
+egrep "^export " ~/.env | while read e
+    set var (echo $e | sed -E "s/^export ([A-Za-z_]+)=(.*)\$/\1/")
+    set value (echo $e | sed -E "s/^export ([A-Za-z_]+)=(.*)\$/\2/")
+
+    # remove surrounding quotes if existing
+    set value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
+
+    if test $var = "PATH"
+        # replace ":" by spaces. this is how PATH looks for Fish
+        set value (echo $value | sed -E "s/:/ /g")
+
+        # use eval because we need to expand the value
+        eval set -xg $var $value
+
+        continue
+    end
+
+    # evaluate variables. we can use eval because we most likely just used "$var"
+    set value (eval echo $value)
+
+    #echo "set -xg '$var' '$value' (via '$e')"
+    set -xg $var $value
+end
