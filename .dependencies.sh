cbdb33ab1b4f515e97e6e79250805ab40d0316ab
rhys <rhysormond@gmail.com>
2019-08-05 07:55:08 -0700

    Break readme into setup and dependencies scripts

--- .dependencies.sh
+++ .dependencies.sh
@@ -1,8 +1,5 @@
-# Configuration Files
-A set of configuration and scripts to roughly recreate my development environment from scratch.
+#!/usr/bin/env bash
 
-## Install Dependencies
-```bash
 pacman -S yay
 yay -S
     alacritty
@@ -34,15 +31,4 @@ yay -S
     xcape
     xfce4-power-manager
     xorg-xbacklight
-```
-
-## Install Configuration
-```bash
-git clone https://github.com/rhysormond/dotfiles.git ~/.cfg --bare
-alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
-config reset
-config stash
-config update
-```
 
