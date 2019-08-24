78d90f7c46a2071a5c91fbafbebe5e43b36cbb30
rhys <rhysormond@gmail.com>
2019-08-23 23:12:43 -0700

    Clean up setup script

--- .setup.sh
+++ .setup.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:rhysormond/dotfiles.git ~/.cfg --bare
-alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
-config reset
-config stash
-config update
+# clones dotfiles into a bare repository $HOME/.cfg
+git clone git@github.com:rhysormond/dotfiles.git $HOME/.cfg --bare
 
+# define a git alias to reference $HOME/.cfg with a work-tree in $HOME
+cfg='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+# hide all the untracked files
+cfg config --local status.showUntrackedFiles no
+
+# reset but keep local changes (reset --hard, stash, or commit what's left)
+cfg reset
