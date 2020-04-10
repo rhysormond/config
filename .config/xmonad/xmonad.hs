ae904cdb79a2729d95e741b9ce201456aeefc4b9
rhysormond <git@rhysormond.com>
2020-04-09 21:32:47 -0700

    style: clean up xmonad spacing

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -20,23 +20,23 @@ myKeys conf@(XConfig { modMask = modm }) =
         ((modm, xK_q),                  spawn "~/.config/rofi/powermenu"),
         ((modm .|. shiftMask, xK_q),    kill),
         ((modm .|. shiftMask, xK_r),    spawn "xmonad --restart"),
-        ((0, xF86XK_AudioMute),
-            spawn "amixer -D pulse sset Master toggle &"),
-        ((0, xF86XK_AudioLowerVolume),
-            spawn "amixer -D pulse sset Master 5%-"),
-        ((0, xF86XK_AudioRaiseVolume),
-            spawn "amixer -D pulse sset Master 5%+"),
+        ((0, xF86XK_AudioMute),         mute),
+        ((0, xF86XK_AudioLowerVolume),  lower),
+        ((0, xF86XK_AudioRaiseVolume),  raise),
         ((0, xF86XK_MonBrightnessDown), spawn "xbacklight - 10 &"),
         ((0, xF86XK_MonBrightnessUp),   spawn "xbacklight + 10 &")
     ]
       where
+        mute       = spawn "amixer -D pulse sset Master toggle &"
+        lower      = spawn "amixer -D pulse sset Master 5%-"
+        raise      = spawn "amixer -D pulse sset Master 5%+"
         scratchpad = namedScratchpadAction scratchpads
 
 myLayoutHook =
     tiled ||| full
       where
-        tiled = smartBorders $ Tall nmaster delta ratio
-        full = withBorder 1 Full
+        tiled   = smartBorders $ Tall nmaster delta ratio
+        full    = withBorder 1 Full
         nmaster = 1
         ratio   = 1 / 2
         delta   = 3 / 100
@@ -45,10 +45,10 @@ scratchpads =
     [NS "term" "alacritty --title 'term'" (title =? "term") floating]
       where
         floating = customFloating $ W.RationalRect left top width height
-        height = 0.75
-        width = 0.60
-        left = 0.20
-        top = 0.00
+        height   = 0.75
+        width    = 0.60
+        left     = 0.20
+        top      = 0.00
 
 myManageHook =
     composeAll [
