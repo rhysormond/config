ba8082278440095557c33b26e48bbc9fbabd11a5
rhysormond <git@rhysormond.com>
2020-07-16 12:06:47 -0700

    feat(fish): improved man page coloring

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,16 +15,16 @@ function pk --description "fzy for a process and kill it"
     end
 end
 
-
-# colored man output
-# from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
-setenv LESS_TERMCAP_mb \e"[01;31m"       # begin blinking
-setenv LESS_TERMCAP_md \e"[01;38;5;74m"  # begin bold
-setenv LESS_TERMCAP_me \e"[0m"           # end mode
-setenv LESS_TERMCAP_se \e"[0m"           # end standout-mode
-setenv LESS_TERMCAP_so \e"[38;5;246m"    # begin standout-mode - info box
-setenv LESS_TERMCAP_ue \e"[0m"           # end underline
-setenv LESS_TERMCAP_us \e"[04;38;5;146m" # begin underline
+function man --description "wrap man to color output without changing less"
+    set -x LESS_TERMCAP_mb (set_color brred)
+    set -x LESS_TERMCAP_md (set_color brred)
+    set -x LESS_TERMCAP_me (set_color normal)
+    set -x LESS_TERMCAP_se (set_color normal)
+    set -x LESS_TERMCAP_so (set_color -b blue bryellow)
+    set -x LESS_TERMCAP_ue (set_color normal)
+    set -x LESS_TERMCAP_us (set_color brgreen)
+  command man $argv
+end
 
 # vi mode key bindings
 fish_vi_key_bindings
