2e2d2ffb6a02871e56d6e8ca926fc753c0f9fb28
rhysormond <git@rhysormond.com>
2022-03-02 23:34:09 -0800

    style(nvim): fix huge indent

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,3 +1,3 @@
 function nnoremap(keys, fn)
-        vim.api.nvim_set_keymap('n', keys, fn, { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', keys, fn, { noremap = true, silent = true })
 end
