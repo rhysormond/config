a6b5eaa4d120977e521ce3eccdb0e7d61d79ae0c
rhysormond <git@rhysormond.com>
2022-12-26 23:59:44 -0800

    feat(nvim): better notifications with noice

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -35,6 +35,13 @@ local plugins = {
     'hoob3rt/lualine.nvim',
     config = function() require 'plugins.lualine' end,
   },
+  {
+    'folke/noice.nvim',
+    config = function() require 'plugins.noice' end,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+  },
 
   -- language server
   {
--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,12 +1,12 @@
-require("noice").setup {
+require 'noice'.setup {
   cmdline = {
     format = {
-      cmdline = { icon = ">" },
-      search_down = { icon = "🔍⌄" },
-      search_up = { icon = "🔍⌃" },
-      filter = { icon = "$" },
-      lua = { icon = "☾" },
-      help = { icon = "?" },
+      cmdline = { icon = '>' },
+      search_down = { icon = '🔍⌄' },
+      search_up = { icon = '🔍⌃' },
+      filter = { icon = '$' },
+      lua = { icon = '☾' },
+      help = { icon = '?' },
     },
   },
 }
