142194daf505f2b55e7afa6df1f95b5ab9c6b41c
rhysormond <git@rhysormond.com>
2022-12-29 20:53:17 -0800

    feat(nvim): add treesitter support for more types

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'lua', 'rust', 'scala' },
+  ensure_installed = { 'lua', 'rust', 'scala', 'vim', 'help' },
 
   highlight = { enable = true },
   indent = { enable = true },
