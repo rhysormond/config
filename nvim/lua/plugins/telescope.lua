fae69cd6f7ab4aac4408994a644a85b9b63ab2ac
rhysormond <git@rhysormond.com>
2023-02-18 20:51:38 -0800

    feat(nvim): <Leader>ff to list git files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ require 'utils'
 nnoremap('[f]ind [k]eymap', '<Leader>fk', ':Telescope keymaps <CR>')
 nnoremap('[f]ind lines within [a]ll files in the project', '<Leader>fa', require 'telescope.builtin'.live_grep)
 nnoremap('[f]ind [l]ines in the open buffer', '<Leader>fl', require 'telescope.builtin'.current_buffer_fuzzy_find)
-nnoremap('[f]ind [f]iles in the project', '<Leader>ff', require 'telescope.builtin'.find_files)
+nnoremap('[f]ind git [f]iles in the project', '<Leader>ff', require 'telescope.builtin'.git_files)
 nnoremap('[f]ind [b]uffers', '<Leader>fb', require 'telescope.builtin'.buffers)
 
 -- use native fzf to improve performance
